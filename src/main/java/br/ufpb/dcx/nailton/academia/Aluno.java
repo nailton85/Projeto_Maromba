f42fa739db909c6a9dd55a65bc59d3d88c7da421
nailton.pereira <pnailton720@gmail.com>
2024-08-29 09:02:26 -0300

    Adicionando imagens para animação

--- src/main/java/br/ufpb/dcx/nailton/academia/Aluno.java
+++ src/main/java/br/ufpb/dcx/nailton/academia/Aluno.java
@@ -1,4 +1,10 @@
 package br.ufpb.dcx.nailton.academia;
 
+import java.util.List;
 public class Aluno {
+    private String nome;
+    private String email;
+    private List<Treino> treino;
+    private String nomeroMatricula;
+
 }
--- src/main/java/br/ufpb/dcx/nailton/academia/Treino.java
+++ src/main/java/br/ufpb/dcx/nailton/academia/Treino.java
@@ -1,4 +1,8 @@
 package br.ufpb.dcx.nailton.academia;
 
 public class Treino {
+    private String nome;
+    private String imagenIlustrativa;
+    private String descricao;
+
 }
