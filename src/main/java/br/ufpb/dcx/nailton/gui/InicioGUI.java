1b1c063fe4b4699013fb4c44750daa94eeeb527d
nailton.pereira <pnailton720@gmail.com>
2024-09-09 08:26:25 -0300

    add

---
+++ src/main/java/br/ufpb/dcx/nailton/gui/InicioGUI.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.nailton.gui;
+
+import javax.swing.*;
+
+public class InicioGUI extends JFrame {
+    JLabel linha1, linha2;
+    ImageIcon tela = new ImageIcon("./img/academia.jpg");
+
+    public InicioGUI(){
+        setTitle("aaaaaaa");
+    }
+
+}
