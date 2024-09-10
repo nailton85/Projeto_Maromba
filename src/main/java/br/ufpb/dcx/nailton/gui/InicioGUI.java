b157c2729f65bc5063a3c5958d8aeb31313b3864
nailton.pereira <pnailton720@gmail.com>
2024-09-10 07:49:47 -0300

    add pesos

--- src/main/java/br/ufpb/dcx/nailton/gui/InicioGUI.java
+++ src/main/java/br/ufpb/dcx/nailton/gui/InicioGUI.java
@@ -1,13 +1,17 @@
 package br.ufpb.dcx.nailton.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class InicioGUI extends JFrame {
     JLabel linha1, linha2;
     ImageIcon tela = new ImageIcon("./img/academia.jpg");
 
     public InicioGUI(){
-        setTitle("aaaaaaa");
+        setTitle("Aguma coisa");
+        setSize(800, 600); //tamanho da janela
+        setLocation(150, 150);
+        setResizable(false);
     }
 
 }
